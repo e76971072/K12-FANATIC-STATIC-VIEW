169ab8110e247a55b143eaef42a2e53e6ee48aaf
Boss of Siri <macbookpro@macbooks-MacBook-Pro-2.local>
2019-10-24 00:36:42 -0500

    Icon adding

---
+++ js/script.js
@@ -0,0 +1,41 @@
+import CircleType from `circletype`;
+$(document).ready(function() {
+    //$('.Highschool').click(HighschoolFunction); 
+    $('.name-jersey').arctext({radius:135});
+}); 
+
+
+function HighschoolFunction (){
+    var arrayImg = ["cedar_park.png"
+                    ,"glen.png"
+                    ,"lion.png"
+                    ,"rouse.png"
+                    ,"vipers.png"
+                    ,"vista-ridge.png"];
+    $.each (arrayImg, function (index, value){
+        
+    })
+}
+
+function displayShippingContainer (){
+   var toggleName = document.getElementById("shipping-01");
+   if ( toggleName.style.display === "none"){
+    toggleName.style.display = "block"; 
+   }
+   else{
+       toggleName.style.display = "none"; 
+   }
+  
+
+}
+function displayShippingContainer2 (){
+    var toggleName = document.getElementById("gender");
+    if ( toggleName.style.display === "none"){
+     toggleName.style.display = "block"; 
+    }
+    else{
+        toggleName.style.display = "none"; 
+    }
+   
+ 
+ }
